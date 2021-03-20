a3a10d8f6cf79c5b36fe2e35c100a61339c0b6a9
Carlos Jimenez <carlos.jimenez@rokk3rlabs.com>
2021-03-19 19:19:07 -0500

    make password nullable to login with github

--- models/user.py
+++ models/user.py
@@ -12,7 +12,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80))
     email = db.Column(db.String(80), nullable=False, unique=True)
 
     confirmation = db.relationship(
